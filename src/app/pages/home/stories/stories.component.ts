3c5df0cea5f66a4c33c570db4d31754eb8da5e18
Rodrigo Iván Miranda Reidenbach <rodrigomreidenbach@gmail.com>
2024-01-01 14:35:47 -0300

    refactorizando el algoritmo de stories

--- src/app/pages/home/stories/stories.component.ts
+++ src/app/pages/home/stories/stories.component.ts
@@ -7,8 +7,12 @@ import { stories } from '../../../models/storiesInterface';
   templateUrl: './stories.component.html',
   styleUrls: ['./stories.component.css']
 })
+//el userservice en esta clase se usa para extraer las storias en los usuarios creados por el mismo servicio.
+//el userservice posee una variable que guarda el perfil del usuario que el usuario final está viendo o quiere ver, ya sea post, storie, o feed post. 
+//la variable antes mencionada se encuentra siendo usada en la función changeStorie() de esta clase
 export class StoriesComponent implements OnInit {
-  storieActual: any= []
+  //cambiar storieActual por "userStories"
+  storieActual: any 
   storiesToShow: { [user: string]: stories[] }[] = [];
   storiesCount: number=0
   storieProgress: string;
@@ -39,30 +43,30 @@ export class StoriesComponent implements OnInit {
 
 
   async openStoriesModal(nombre: string) {
-    this.storieActual= await this.storiesToShow.find((obj) => obj[nombre][0]);
+    //TOMAR LAS STORIES DEL USUARIO PARA MOSTRARLAS
+    this.storieActual=await this.storiesToShow.find((obj) => obj[nombre]);
+    console.log("storie actual al abrir el modal:",this.storieActual)
+    for (let index = 0; index < this.storieActual.length; index++) {
+   console.log(index)
+      
+    }
     if(this.modalStorie != undefined){
       this.modalStorie.nativeElement.style.display = 'block';
     }
   }
-  changeStorie(nombre: string) {
-    const currentIndex = this.storiesToShow.findIndex((obj) => obj[nombre]);
+  changeStorie() {
+    const nombreUsuario = Object.keys(this.storieActual)[0];
+    const currentIndex = this.storiesToShow.findIndex((obj) => obj[nombreUsuario]);
   //eliminar nombre. buscar automaticamente al siguiente usuario para mostrar sus historias y saltar al ultimo y mostrar sus historias igualmente.
     if (currentIndex !== -1) {
-      const nextUser = this.storiesToShow[currentIndex + 1]?.[nombre];
-      
+      const nextUser = this.storiesToShow[currentIndex + 1];  
       if (nextUser) {
-        console.log('Usuario siguiente:', nextUser);
         // Realiza acciones con el siguiente usuario si es necesario
+        this.storieActual=nextUser
       } else {
         // Vuelve al primer usuario
         const primerUsuario = this.storiesToShow[0];
         this.storieActual= primerUsuario;
-        if (primerUsuario) {
-          console.log('Volviendo al primer usuario:', primerUsuario);
-          // Realiza acciones con el primer usuario si es necesario
-        } else {
-          console.log('No hay usuario en la lista');
-        }
       }
     } else {
       console.log('Usuario no encontrado');
