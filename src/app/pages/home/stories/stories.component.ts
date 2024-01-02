3223847f30ac8137affb4cc7e8dfa312bd22881b
Rodrigo Iván Miranda Reidenbach <rodrigomreidenbach@gmail.com>
2024-01-01 21:15:33 -0300

    working in stories and addind scss

--- src/app/pages/home/stories/stories.component.ts
+++ src/app/pages/home/stories/stories.component.ts
@@ -5,77 +5,59 @@ import { stories } from '../../../models/storiesInterface';
 @Component({
   selector: 'app-stories',
   templateUrl: './stories.component.html',
-  styleUrls: ['./stories.component.css']
+  styleUrls: ['./stories.component.scss']
 })
-//el userservice en esta clase se usa para extraer las storias en los usuarios creados por el mismo servicio.
-//el userservice posee una variable que guarda el perfil del usuario que el usuario final está viendo o quiere ver, ya sea post, storie, o feed post. 
-//la variable antes mencionada se encuentra siendo usada en la función changeStorie() de esta clase
 export class StoriesComponent implements OnInit {
-  //cambiar storieActual por "userStories"
-  storieActual: any 
-  storiesToShow: { [user: string]: stories[] }[] = [];
-  storiesCount: number=0
+  userStories: { [user: string]: stories[] }[] = [];
+  storieActual: any;
   storieProgress: string;
   storiesVistas: stories[] = [];
   loading: boolean;
-  @ViewChild('modalStorie') modalStorie?: ElementRef | undefined;
+  @ViewChild('modalStorie') modalStorie?: ElementRef;
 
   constructor(public userService: UserService) {
     this.loading = false;
-
-    this.userService.usuarios.forEach((element) => {
-      let newUser = element.nombre;
-      let userToInsert: { [user: string]: stories[] } = {};
-      userToInsert[newUser] = [];
-
-      element.stories.forEach((element2) => {
-        userToInsert[newUser].push(element2);
-      });
-
-      this.storiesToShow.push(userToInsert);
-    });
-
+    this.loadUserStories();
     this.loading = true;
     this.storieProgress = '';
   }
 
   ngOnInit() {}
 
-
   async openStoriesModal(nombre: string) {
-    //TOMAR LAS STORIES DEL USUARIO PARA MOSTRARLAS
-    this.storieActual=await this.storiesToShow.find((obj) => obj[nombre]);
-    console.log("storie actual al abrir el modal:",this.storieActual)
-    for (let index = 0; index < this.storieActual.length; index++) {
-   console.log(index)
-      
-    }
-    if(this.modalStorie != undefined){
+    //antes
+    //this.storieActual = this.userStories.find((obj) => obj[nombre])
+    //gpt quita trabajos:
+    this.storieActual = this.userStories.find((obj) => obj[nombre])?.[nombre] || [];
+    console.log("storie actual al abrir el modal:", this.storieActual);
+
+    if (this.modalStorie) {
       this.modalStorie.nativeElement.style.display = 'block';
     }
   }
+
   changeStorie() {
-    const nombreUsuario = Object.keys(this.storieActual)[0];
-    const currentIndex = this.storiesToShow.findIndex((obj) => obj[nombreUsuario]);
-  //eliminar nombre. buscar automaticamente al siguiente usuario para mostrar sus historias y saltar al ultimo y mostrar sus historias igualmente.
+    const currentIndex = this.userStories.indexOf(this.storieActual);
+
+    // Eliminar nombre, buscar automáticamente al siguiente usuario para mostrar sus historias
+    // y saltar al último y mostrar sus historias igualmente.
     if (currentIndex !== -1) {
-      const nextUser = this.storiesToShow[currentIndex + 1];  
-      if (nextUser) {
-        // Realiza acciones con el siguiente usuario si es necesario
-        this.storieActual=nextUser
-      } else {
-        // Vuelve al primer usuario
-        const primerUsuario = this.storiesToShow[0];
-        this.storieActual= primerUsuario;
-      }
+      const nextUser = this.userStories[currentIndex + 1];
+      this.storieActual = nextUser || this.userStories[0];
     } else {
       console.log('Usuario no encontrado');
     }
   }
-  
+
   closeStoriesModal() {
-    if (this.modalStorie != undefined) {
+    if (this.modalStorie) {
       this.modalStorie.nativeElement.style.display = 'none';
     }
+  }
+
+  private loadUserStories() {
+    this.userStories = this.userService.usuarios.map((user) => ({
+      [user.nombre]: user.stories
+    }));
   }
 }
