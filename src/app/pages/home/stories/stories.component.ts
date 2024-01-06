96c476cf7a712fffba42b4561b3e3272d577f98c
Rodrigo Iván Miranda Reidenbach <rodrigomreidenbach@gmail.com>
2024-01-06 18:20:46 -0300

    i'm not to solve the stories issue for now

--- src/app/pages/home/stories/stories.component.ts
+++ src/app/pages/home/stories/stories.component.ts
@@ -10,6 +10,7 @@ import { stories } from '../../../models/storiesInterface';
 })
 export class StoriesComponent implements OnInit {
   storieViendose: stories | null;
+  private storieTimeout: any;
   userStories: { [user: string]: stories[] }[] = [];
   storiesDelUsuarioEscogido: stories[] = [];
   storieProgress = '';
@@ -20,7 +21,6 @@ export class StoriesComponent implements OnInit {
   constructor(public userService: UserService) {
     this.storieViendose = null;
     this.loading = true;
-    this.storieProgress = '';
     this.loadUserStories();
   }
 
@@ -29,7 +29,6 @@ export class StoriesComponent implements OnInit {
   openStoriesModal(nombre: string) {
     this.storiesDelUsuarioEscogido = this.userStories.find((obj) => obj[nombre])?.[nombre] || [];
     this.storieViendose = this.storiesDelUsuarioEscogido[0];
-    console.log("storie actual al abrir el modal:", this.storiesDelUsuarioEscogido);
 
     if (this.modalStorie) {
       this.modalStorie.nativeElement.style.display = 'block';
@@ -37,20 +36,17 @@ export class StoriesComponent implements OnInit {
     this.playUserStories();
   }
 
-  changeStorie() {
+  async changeStorie() {
     if (!this.storiesDelUsuarioEscogido) {
       console.log('No hay historias para el usuario actual');
       return;
     }
 
     const currentIndex = this.storiesDelUsuarioEscogido.findIndex((storie) => storie.id === this.storieViendose?.id);
-    console.log("indice antes de ejecutar changeusername:", currentIndex);
 
     if (currentIndex !== -1) {
       const nextIndex = (currentIndex + 1) % this.storiesDelUsuarioEscogido.length;
-
-      console.log("indice a recorrer:", nextIndex);
-
+      this.cancelStorie();
       this.storieViendose = this.storiesDelUsuarioEscogido[nextIndex];
       this.playUserStories();
     } else {
@@ -67,21 +63,21 @@ export class StoriesComponent implements OnInit {
   private loadUserStories() {
     this.userStories = this.userService.usuarios.map((user) => ({ [user.nombre]: user.stories }));
   }
-
   playUserStories() {
     if (this.storieViendose) {
+      this.storieViendose.seen = false;
       this.storieViendose.progress = '0%';
-
-      setTimeout(() => {
+      this.storieTimeout = setTimeout(() => {
         if (this.storieViendose) {
           this.storieViendose.progress = '100%';
           this.storieViendose.seen = true;
           this.changeStorie();
         }
-
       }, 3000);
-
-    
     }
+  }
+
+  cancelStorie() {
+    clearTimeout(this.storieTimeout);
   }
 }
